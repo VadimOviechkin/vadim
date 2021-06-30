be5945554ed7b882ef139874dd18b3bf78532fef
Vladyslav_Tsiupa <vladyslav_tsiupa@epam.com>
2021-06-30 17:52:51 +0300

    Refactored, but code is still shitty

--- Part10.java
+++ Part10.java
@@ -1,11 +1,12 @@
 public class Part10 {
 
     public static void main(String[] args) {
-        int[] array = new int[Integer.parseInt(args[0])];
+        //int arrayLength = Integer.parseInt(args[0]);
+        int arrayLength = 10;//for testing
+        int[] array = new int[arrayLength];
         int temp;
-        int some = 0;
-        boolean isPrime = true;
-        for (int i = 2, n = 0; i <= Integer.parseInt(args[0]); i++) {
+        boolean isPrime;
+        for (int i = 1, n = 0; n <= arrayLength - 1; i++) {
             isPrime = true;
             for (int f = 2; f <= i / 2; f++) {
                 temp = i % f;
@@ -13,19 +14,18 @@ public class Part10 {
                     isPrime = false;
                 }
             }
+            if (i == 2) {
+                isPrime = false;
+            }
             if (isPrime) {
                 array[n] = i;
                 n++;
-                some++;
             }
         }
-        System.out.print(some);
-        for(int n = 0; n >= some; n++){
-            if (n == some){
-                System.out.print(array[n]);
-            } else {
-                System.out.print(array[n] + " ");
-            }
+
+        for (int element : array) {
+            if (element == 0) break;
+            System.out.print(element + " ");
         }
     }
 }
